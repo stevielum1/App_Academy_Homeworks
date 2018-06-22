6d6dd1264aa86ee20d53f1aca33fd433e1e89bcd
Steven Lum <stevielum1@gmail.com>
2018-06-21 21:51:13 -0700

    Add stack class

---
+++ w1d5/stack.rb
@@ -0,0 +1,19 @@
+class Stack
+
+   def initialize(stack = [])
+     @stack = stack
+   end
+
+   def push(el)
+     @stack << el
+   end
+
+   def pop
+     @stack.pop
+   end
+
+   def peek
+     @stack.last
+   end
+   
+ end

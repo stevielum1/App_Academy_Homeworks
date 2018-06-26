01354f91760245bd1cf102f13ca9db6e6293e9a8
Steven Lum <stevielum1@gmail.com>
2018-06-25 21:29:53 -0700

    Update Board class methods

--- w2d2/lib/board.rb
+++ w2d2/lib/board.rb
@@ -23,18 +23,20 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
-    stones = cups[start_pos].dup
+    stones = cups[start_pos]
     cups[start_pos] = []
 
-    current_pos = (start_pos + 1) % cups.length
+    current_pos = start_pos
 
     until stones.empty?
-      if current_player_name == name1
-        cups[current_pos] << stones.pop unless current_pos == 13
-      else
-        cups[current_pos] << stones.pop unless current_pos == 6
-      end
       current_pos = (current_pos + 1) % cups.length
+      if current_pos == 6
+        cups[current_pos] << stones.pop if current_player_name == name1
+      elsif current_pos == 13
+        cups[current_pos] << stones.pop if current_player_name == name2
+      else
+        cups[current_pos] << stones.pop
+      end
     end
 
     render
@@ -44,6 +46,13 @@ class Board
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif cups[ending_cup_idx].length == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -55,9 +64,20 @@ class Board
   end
 
   def one_side_empty?
+    return true if cups[0...6].all? { |cup| cup.empty? }
+    return true if cups[7...cups.length-1].all? { |cup| cup.empty? }
+    false
   end
 
   def winner
+    case cups[6] <=> cups[13]
+    when -1
+      name2
+    when 0
+      :draw
+    when 1
+      name1
+    end
   end
 
   private

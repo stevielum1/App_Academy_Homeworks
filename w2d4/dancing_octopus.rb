cc966d53f77e3cdfa9a41fb7be026c54fe407fa3
Steven Lum <stevielum1@gmail.com>
2018-06-27 12:59:43 -0700

    Add fast dance

--- w2d4/dancing_octopus.rb
+++ w2d4/dancing_octopus.rb
@@ -1,6 +1,17 @@
 tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
 
+tiles_hash = {}
+
+tiles_array.each_with_index do |tile, i|
+  tiles_hash[tile] = i
+end
+
 # O(n)
 def slow_dance(move, tiles_array)
   tiles_array.each_with_index { |tile, i| return i if tile == move }
+end
+
+# O(1)
+def fast_dance(move, tiles_hash)
+  tiles_hash[move]
 end

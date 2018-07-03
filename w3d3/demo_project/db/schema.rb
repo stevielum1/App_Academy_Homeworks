689b4c6e11d297d73fca41a9802ceb6a8115edb4
Steven Lum <stevielum1@gmail.com>
2018-07-03 09:32:10 -0700

    Add person and house associations

--- w3d3/demo_project/db/schema.rb
+++ w3d3/demo_project/db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_160527) do
+ActiveRecord::Schema.define(version: 2018_07_03_161343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "address"
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name"

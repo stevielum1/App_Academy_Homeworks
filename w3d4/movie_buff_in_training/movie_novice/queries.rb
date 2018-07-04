c2efe1b2695833f291a4cc84ca9bc47b5a54b134
Steven Lum <stevielum1@gmail.com>
2018-07-03 20:56:29 -0700

    Complete movie_buff_in_training

--- w3d4/movie_buff_in_training/movie_novice/queries.rb
+++ w3d4/movie_buff_in_training/movie_novice/queries.rb
@@ -23,19 +23,19 @@
 
 def find_angelina
   #find Angelina Jolie by name in the actors table
-
+  Actor.find_by_name('Angelina Jolie')
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-
+  Movie.where('score >= ?', 9).select('id, title')
 end
 
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
-
+  Movie.where('title LIKE ?', 'Star Wars%').select('id, title, yr')
 end
 
 
@@ -44,7 +44,11 @@ def below_average_years
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
-
+  Movie
+    .where('score < ?', 5)
+    .group('yr')
+    .select('yr, COUNT(*) AS bad_movies')
+    .order('yr DESC')
 end
 
 def alphabetized_actors
@@ -53,14 +57,18 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-
+  Actor.order('name ASC').limit(10)
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Actor
+    .joins(:castings)
+    .joins(:movies)
+    .where('movies.title = ?', 'Pulp Fiction')
+    .select('DISTINCT actors.id, actors.name')
 end
 
 def uma_movies
@@ -68,5 +76,10 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-
+  Movie
+    .joins(:castings)
+    .joins(:actors)
+    .where('actors.name = ?', 'Uma Thurman')
+    .select('DISTINCT movies.id, movies.title, movies.yr')
+    .order('yr ASC')
 end

7eacf1187828c7415772150bd4c437076b04658a
Steven Lum <stevielum1@gmail.com>
2018-07-11 13:01:25 -0700

    Add letter_opener gem

--- w4d4/config/environments/development.rb
+++ w4d4/config/environments/development.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.deliver_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 

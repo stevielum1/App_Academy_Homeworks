e1f9e09d80dfd8b0b5fbcae7b8c32ea22cd09038
Steven Lum <stevielum1@gmail.com>
2018-07-18 10:44:41 -0700

    Finish phase 3 of w5d4 part2

--- w5d4/intro_js_part2.js
+++ w5d4/intro_js_part2.js
@@ -33,3 +33,18 @@ Elephant.prototype.play = function() {
   let trick = this.tricks[ Math.floor(Math.random() * this.tricks.length) ];
   console.log(`${this.name} is ${trick}`);
 }
+
+//Phase 3 Function Invocation
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+let herd = [ellie, charlie, kate, micah];
+
+Elephant.paradeHelper = function(elephant) {
+  console.log(`${elephant.name} is trotting by!`);
+}
+
+herd.forEach(Elephant.paradeHelper);
+
+//Phase 4 Closures

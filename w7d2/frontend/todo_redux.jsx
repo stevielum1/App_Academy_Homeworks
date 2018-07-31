0a2de83d902dae45d90a63fc93baf731c35fdb10
Steven Lum <stevielum1@gmail.com>
2018-07-30 21:21:48 -0700

    Add w7d2 hw

--- w7d2/frontend/todo_redux.jsx
+++ w7d2/frontend/todo_redux.jsx
@@ -7,8 +7,37 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+  let store = configureStore(preloadedState);
+
+  // store.dispatch = addLoggingToDispatch(store);
+
+  // store = applyMiddlewares(store, addLoggingToDispatch);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+// function addLoggingToDispatch(store) {
+//   const dispatch = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     dispatch(action);
+//     console.log(store.getState());
+//   };
+// }
+
+// const addLoggingToDispatch = store => next => action => {
+//   console.log(store.getState());
+//   console.log(action);
+//   next(action);
+//   console.log(store.getState());
+// };
+
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach(middleware => {
+//     dispatch = middleware(store)(dispatch);
+//   });
+//   return Object.assign({}, store, { dispatch });
+// };

15f7d401dcae5b7c0cd82e0639e7f05fa2e8d71a
Steven Lum <stevielum1@gmail.com>
2018-08-01 12:53:48 -0700

    Add w7d3 giphy hw

--- w7d3/giphy/components/giphys_index.jsx
+++ w7d3/giphy/components/giphys_index.jsx
@@ -1,3 +1,17 @@
 import React from 'react';
 
 import GiphysIndexItem from './giphys_index_item';
+
+const GiphysIndex = ({giphys}) => {
+  return (
+    <div>
+      <ul>
+        {
+          giphys.map(giphy => <GiphysIndexItem giphy={giphy} key={giphy.id} />)
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default GiphysIndex;
--- w7d3/giphy/components/giphys_search.jsx
+++ w7d3/giphy/components/giphys_search.jsx
@@ -1,3 +1,39 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    const searchTerm = e.currentTarget.value;
+    this.setState({ searchTerm });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { searchTerm } = this.state
+    this.props.fetchSearchGiphys(searchTerm);
+    this.setState({ searchTerm: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange} value={this.state.searchTerm}/>
+        <button onClick={this.handleSubmit}>Submit</button>
+        <GiphysIndex giphys={this.props.giphys}/>
+      </div>
+    );
+  }
+}
+
+export default GiphysSearch;
